5a630e3cb710bdd0a7ef6c29a1487d47bb423350
rajireddymula <rajireddym@live.com>
2021-10-20 23:41:09 +0100

    update

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed another!",
+        "message": "Go Serverless v1.0! Your function executed another!!",
         "input": event
     }
 
